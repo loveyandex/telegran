fb29926292bb05f7dcb502172666ac3fefb74977
Lusics Mayrics <abbfzl@gmail.com>
2021-04-08 13:19:36 +0430

    D

---
+++ otherapp/chats.py
@@ -0,0 +1,45 @@
+import pickle
+
+from telethon import TelegramClient, events, sync
+from telethon.tl import functions
+from telethon.tl.functions.channels import InviteToChannelRequest
+
+api_id = 95576
+api_hash = '0bcee09d8c8beb2e7c0d52ce4c1ef594'
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
+
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+
+chats = []
+last_date = None
+chunk_size = 200
+groups = []
+
+result = client(GetDialogsRequest(
+    offset_date=last_date,
+    offset_id=0,
+    offset_peer=InputPeerEmpty(),
+    limit=chunk_size,
+    hash=0
+))
+
+chats.extend(result.chats)
+
+megagroups = []
+
+for chat in chats:
+    if hasattr(chat, 'megagroup') and chat.megagroup:
+        megagroups.append(chat)
+
+for megag in megagroups:
+    if megag.title == 'تحلیل بازارهای مالی':
+        print('Fetching Members...')
+        all_participants = []
+        all_participants = client.get_participants(megag, aggressive=True)
+
+        with open(str(megag.id) + "__" + megag.title + ".usrs", "wb") as fp:  # Pickling
+            pickle.dump(all_participants, fp)
+
+        print(len(all_participants))
